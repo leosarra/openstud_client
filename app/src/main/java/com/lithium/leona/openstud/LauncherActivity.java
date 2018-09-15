31255bc1c6145eeb56895abd03accb6befb985e8
Leonardo Sarra <8293810+LithiumSR@users.noreply.github.com>
2018-09-16 00:01:20 +0200

    LauncherActivity: Upload launcher activity

---
+++ app/src/main/java/com/lithium/leona/openstud/LauncherActivity.java
@@ -0,0 +1,35 @@
+package com.lithium.leona.openstud;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lithium.leona.openstud.data.InfoManager;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        //Intent int2 = new Intent(this,PaymentsActivity.class);
+        //startActivity(int2);
+        if (!InfoManager.getSaveFlag(getApplication())) InfoManager.clearSharedPreferences(getApplication());
+        if (InfoManager.hasLogin(getApplication())) {
+            Intent intent = new Intent(LauncherActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Intent intent = new Intent(LauncherActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        if (!InfoManager.getSaveFlag(getApplication())) {
+            InfoManager.clearSharedPreferences(getApplication());
+        }
+    }
+}
