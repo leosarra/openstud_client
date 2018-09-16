517397ba051b93dac9e28166d43f67c652ae5981
Leonardo Sarra <8293810+LithiumSR@users.noreply.github.com>
2018-09-16 11:34:18 +0200

    PaymentsFragment: Tentative changes to support swipe to refresh

--- app/src/main/java/com/lithium/leona/openstud/fragments/PaymentsFragment.java
+++ app/src/main/java/com/lithium/leona/openstud/fragments/PaymentsFragment.java
@@ -1,6 +1,12 @@
 package com.lithium.leona.openstud.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.UiThread;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,24 +17,34 @@ import android.view.ViewGroup;
 
 import android.support.annotation.Nullable;
 
+import com.lithium.leona.openstud.LauncherActivity;
+import com.lithium.leona.openstud.PaymentsActivity;
+import com.lithium.leona.openstud.ProfileActivity;
 import com.lithium.leona.openstud.R;
 import com.lithium.leona.openstud.adapters.TaxAdapter;
+import com.lithium.leona.openstud.data.InfoManager;
+import com.lithium.leona.openstud.helpers.ClientHelper;
 
 import org.threeten.bp.LocalDate;
 
+import java.lang.ref.WeakReference;
 import java.util.LinkedList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import lithium.openstud.driver.core.Openstud;
 import lithium.openstud.driver.core.Tax;
+import lithium.openstud.driver.exceptions.OpenstudConnectionException;
+import lithium.openstud.driver.exceptions.OpenstudInvalidResponseException;
 
 public class PaymentsFragment extends android.support.v4.app.Fragment {
-    @BindView(R.id.swipe_refresh) SwipeRefreshLayout swipeRefresh;
+    @BindView(R.id.swipe_refresh) SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.recyclerView) RecyclerView rv;
     private List<Tax> taxes;
-    private RecyclerView rv;
     private TaxAdapter adapter;
     private int mode;
+    private Openstud os;
 
     @Nullable
     @Override
@@ -37,101 +53,63 @@ public class PaymentsFragment extends android.support.v4.app.Fragment {
         ButterKnife.bind(this, v);
         Bundle bundle=getArguments();
         mode = bundle.getInt("mode");
-        rv = (RecyclerView)v.findViewById(R.id.recyclerView);
+        taxes = new LinkedList<>();
+        os = InfoManager.getOpenStud(getActivity().getApplication());
+        if (mode == TaxAdapter.Mode.PAID.getValue()) taxes = InfoManager.getPaidTaxesCached(getActivity().getApplication(),os);
+        else if (mode == TaxAdapter.Mode.UNPAID.getValue()) taxes = InfoManager.getUnpaidTaxesCached(getActivity().getApplication(),os);
+        if (taxes == null)  taxes = new LinkedList<>();
+        if (os == null) {
+            InfoManager.clearSharedPreferences(getActivity().getApplication());
+            Intent i = new Intent(getActivity(), LauncherActivity.class);
+            startActivity(i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        }
         rv.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         rv.setLayoutManager(llm);
-        taxes = new LinkedList<>();
         adapter = new TaxAdapter(getActivity(), taxes, mode);
         rv.setAdapter(adapter);
-        //createListData();
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
+        {
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
+
+        refresh();
         return v;
     }
 
-    /**
-    private void createListData() {
-        Tax tax = new Tax();
-        tax.setCode("201900010747164");
-        tax.setAcademicYear(20128);
-        tax.setAmount(21);
-        tax.setDescriptionCourse("Lorem ipsum docet");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("ISCRIZIONE");
-        taxes.add(tax);
 
-        tax = new Tax();
-        tax.setCode("201900010747164");
-        tax.setAcademicYear(20128);
-        tax.setAmount(22321);
-        tax.setDescriptionCourse("Lorem ipsum docet2");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("IMMATRICOLAZIONE");
-        taxes.add(tax);
 
-        tax = new Tax();
-        tax.setCode("201900010747164");
-        tax.setAcademicYear(20128);
-        tax.setAmount(43321);
-        tax.setDescriptionCourse("Lorem ipsum docet3");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("TASSE");
-        taxes.add(tax);
-
-        tax = new Tax();
-        tax.setCode("44111111231");
-        tax.setAcademicYear(20128);
-        tax.setAmount(6321);
-        tax.setDescriptionCourse("Lorem ipsum docet3");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("TASSE");
-        taxes.add(tax);
-
-        tax = new Tax();
-        tax.setCode("44111111231");
-        tax.setAcademicYear(20128);
-        tax.setAmount(1321);
-        tax.setDescriptionCourse("Ciao4");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("TASSE");
-        taxes.add(tax);
-
-        tax = new Tax();
-        tax.setCode("44111111231");
-        tax.setAcademicYear(20128);
-        tax.setAmount(2321);
-        tax.setDescriptionCourse("Ciao4");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("TASSE");
-        taxes.add(tax);
-
-        tax = new Tax();
-        tax.setCode("44111111231");
-        tax.setAcademicYear(20128);
-        tax.setAmount(4321);
-        tax.setDescriptionCourse("Ciao4");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("TASSE");
-        taxes.add(tax);
-
-        tax = new Tax();
-        tax.setCode("44111111231");
-        tax.setAcademicYear(20128);
-        tax.setAmount(1321);
-        tax.setDescriptionCourse("Ciao4");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("TASSE");
-        taxes.add(tax);
-
-        tax = new Tax();
-        tax.setCode("44111111231");
-        tax.setAcademicYear(20128);
-        tax.setAmount(321);
-        tax.setDescriptionCourse("Ciao4");
-        tax.setPaymentDate(LocalDate.now());
-        tax.setPaymentDescriptionList("TASSE");
-        taxes.add(tax);
-
-        adapter.notifyDataSetChanged();
+    private synchronized void  refresh(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(true);
+                List<Tax> update = null;
+                try {
+                    if (mode == TaxAdapter.Mode.PAID.getValue()) update = InfoManager.getPaidTaxes(getActivity().getApplication(),os);
+                    else if (mode == TaxAdapter.Mode.UNPAID.getValue()) update = InfoManager.getUnpaidTaxes(getActivity().getApplication(),os);
+                } catch (OpenstudConnectionException e) {
+                    e.printStackTrace();
+                } catch (OpenstudInvalidResponseException e) {
+                    e.printStackTrace();
+                } finally {
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+                System.out.println(update);
+                taxes.clear();
+                taxes.addAll(update);
+                System.out.println(taxes);
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }).start();
     }
-    **/
+
 }
