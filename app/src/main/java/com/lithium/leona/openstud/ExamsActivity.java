70613e3d4bb18dc2f557cde71de1e86124d23f35
Leonardo Sarra <8293810+LithiumSR@users.noreply.github.com>
2018-09-17 00:14:41 +0200

    ExamActivity: Add a placeholder activity as a new main activity

---
+++ app/src/main/java/com/lithium/leona/openstud/ExamsActivity.java
@@ -0,0 +1,127 @@
+package com.lithium.leona.openstud;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lithium.leona.openstud.data.InfoManager;
+import com.lithium.leona.openstud.helpers.LayoutHelper;
+import com.lithium.leona.openstud.listeners.DelayedDrawerListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ExamsActivity extends AppCompatActivity {
+    @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    private TextView mTextMessage;
+    private NavigationView nv;
+    private DelayedDrawerListener ddl;
+    private View headerLayout;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exams);
+        ButterKnife.bind(this);
+        nv = LayoutHelper.setupNavigationDrawer(this, mDrawerLayout);
+        LayoutHelper.setupToolbar(this, toolbar, R.drawable.ic_baseline_menu);
+        getSupportActionBar().setTitle(R.string.exams);
+        headerLayout = nv.getHeaderView(0);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setupListeners();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void setupListeners(){
+        ddl = new DelayedDrawerListener(){
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+                int item = getItemPressedAndReset();
+                if (item == -1) return;
+                switch (item) {
+                    case R.id.payments_menu: {
+                        Intent intent = new Intent(ExamsActivity.this, PaymentsActivity.class);
+                        startActivity(intent);
+                        break;
+                    }
+
+                    case R.id.profile_menu: {
+                        Intent intent = new Intent(ExamsActivity.this, ProfileActivity.class);
+                        startActivity(intent);
+                        break;
+                    }
+
+                    case R.id.exit_menu: {
+                        InfoManager.clearSharedPreferences(getApplication());
+                        Intent i = new Intent(ExamsActivity.this, LauncherActivity.class);
+                        startActivity(i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                        break;
+                    }
+                }
+            }
+
+        };
+        mDrawerLayout.addDrawerListener(ddl);
+        nv.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        mDrawerLayout.closeDrawers();
+                        ddl.setItemPressed(item.getItemId());
+                        return true;
+                    }
+                });
+    }
+}
