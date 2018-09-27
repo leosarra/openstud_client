38cd980c7ad0a5c1424060c67495f612dc946fad
Leonardo Sarra <leonardosarra@outlook.com>
2018-09-27 21:33:49 +0200

    About: Tentative changes to support dark theme

--- app/src/main/java/com/lithium/leona/openstud/AboutActivity.java
+++ app/src/main/java/com/lithium/leona/openstud/AboutActivity.java
@@ -2,12 +2,16 @@ package com.lithium.leona.openstud;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 
 import com.danielstone.materialaboutlibrary.ConvenienceBuilder;
 import com.danielstone.materialaboutlibrary.MaterialAboutActivity;
@@ -16,6 +20,8 @@ import com.danielstone.materialaboutlibrary.model.MaterialAboutCard;
 import com.danielstone.materialaboutlibrary.model.MaterialAboutList;
 import com.mikepenz.aboutlibraries.Libs;
 import com.mikepenz.aboutlibraries.LibsBuilder;
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.iconics.IconicsDrawable;
 
 public class AboutActivity extends MaterialAboutActivity {
 
@@ -39,39 +45,58 @@ public class AboutActivity extends MaterialAboutActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_MaterialAboutActivityDark);
         super.onCreate(savedInstanceState);
     }
 
     private void buildApp(Context context, MaterialAboutCard.Builder appCardBuilder) {
+        int tintColor = getPrimaryTextColor(this);
+        Drawable version = ContextCompat.getDrawable(context, R.drawable.ic_update_black);
+        version.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP);
+
         appCardBuilder.addItem(new MaterialAboutActionItem.Builder()
                 .text(getResources().getString(R.string.version))
-                .icon(ContextCompat.getDrawable(context, R.drawable.ic_update_black)).subText(BuildConfig.VERSION_NAME).build());
+                .icon(version).subText(BuildConfig.VERSION_NAME).build());
 
     }
 
     private void buildAuthor(Context context, MaterialAboutCard.Builder authorCardBuilder) {
+        int tintColor = getPrimaryTextColor(this);
+        Drawable person = ContextCompat.getDrawable(context, R.drawable.ic_person_outline_black);
+        Drawable github = new IconicsDrawable(this)
+                .icon(FontAwesome.Icon.faw_github)
+                .color(tintColor)
+                .sizeDp(24);
+        Drawable email = ContextCompat.getDrawable(context, R.drawable.ic_email_black);
+        github.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP);
+        email.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP);
+        person.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP);
         authorCardBuilder.title(R.string.author);
         authorCardBuilder.addItem(new MaterialAboutActionItem.Builder()
                 .text("Leonardo Sarra")
                 .subText("LithiumSR")
-                .icon(ContextCompat.getDrawable(context, R.drawable.ic_person_outline_black))
+                .icon(person)
                 .build())
                 .addItem(new MaterialAboutActionItem.Builder()
                         .text(R.string.fork_github)
-                        .icon(ContextCompat.getDrawable(context, R.drawable.ic_github_circle))
+                        .icon(github)
                         .setOnClickAction( ()-> startActivity(createBrowserLink("https://www.github.com/lithiumSR/openstud_client")))
                         .build())
-                .addItem(ConvenienceBuilder.createEmailItem(context, ContextCompat.getDrawable(context, R.drawable.ic_email_black),
+                .addItem(ConvenienceBuilder.createEmailItem(context, email,
                         getString(R.string.send_email), true, getString(R.string.email_address), getString(R.string.question_concerning_openstud)));
     }
 
     private void buildMisc(Context context, MaterialAboutCard.Builder miscCardBuilder) {
+        int tintColor = getPrimaryTextColor(this);
+        Drawable github = new IconicsDrawable(this)
+                .icon(FontAwesome.Icon.faw_github)
+                .color(tintColor)
+                .sizeDp(24);
         miscCardBuilder.title(R.string.about).addItem(new MaterialAboutActionItem.Builder()
                 .text(R.string.open_source_libs)
-                .icon(ContextCompat.getDrawable(context, R.drawable.ic_github_circle))
+                .icon(github)
                 .setOnClickAction(() -> new LibsBuilder()
-                        .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
-                        .withActivityTheme(R.style.NoActionBarAppDarkTheme)
+                        .withActivityStyle(Libs.ActivityStyle.DARK)
                         .withAutoDetect(true)
                         .withActivityTitle(this.getResources().getString(R.string.open_source_libs))
                         .withAboutIconShown(true)
@@ -83,5 +108,15 @@ public class AboutActivity extends MaterialAboutActivity {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(url));
         return i;
+    }
+
+    private int getPrimaryTextColor(Context context){
+        int tintColor;
+        TypedValue tV = new TypedValue();
+        Resources.Theme theme = context.getTheme();
+        boolean success = theme.resolveAttribute(R.attr.primaryTextColor, tV, true);
+        if (success) tintColor = tV.data;
+        else tintColor = ContextCompat.getColor(context, android.R.color.black);
+        return tintColor;
     }
 }
