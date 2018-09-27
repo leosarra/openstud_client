9c5391206d8e7f818fd6ee9f90718842da8c8662
Leonardo Sarra <leonardosarra@outlook.com>
2018-09-27 21:31:52 +0200

    Payments: Add dark theme

--- app/src/main/java/com/lithium/leona/openstud/activities/PaymentsActivity.java
+++ app/src/main/java/com/lithium/leona/openstud/activities/PaymentsActivity.java
@@ -44,6 +44,7 @@ public class PaymentsActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.NoActionBarAppDarkTheme);
         setContentView(R.layout.activity_payments);
         ButterKnife.bind(this);
         os = InfoManager.getOpenStud(this);
