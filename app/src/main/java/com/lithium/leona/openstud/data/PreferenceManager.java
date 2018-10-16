401dd914e8c9d684a95e2620ce6a3648a230e168
Leonardo Sarra <leonardosarra@outlook.com>
2018-10-16 22:57:12 +0200

    PreferenceManager: Add a custom Preference Manager on top of the default one

---
+++ app/src/main/java/com/lithium/leona/openstud/data/PreferenceManager.java
@@ -0,0 +1,37 @@
+package com.lithium.leona.openstud.data;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.lithium.leona.openstud.R;
+
+public class PreferenceManager {
+    private static SharedPreferences pref;
+
+    private static synchronized void setupSharedPreferences(Context context){
+        if (pref!=null) return;
+        pref = android.support.v7.preference.PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static boolean getStatsNotificationEnabled(Context context) {
+        setupSharedPreferences(context);
+        synchronized (PreferenceManager.class) {
+            return pref.getBoolean("statsNotification", true);
+        }
+    }
+
+
+    public static void setStatsNotificationEnabled(Context context, boolean enabled) {
+        setupSharedPreferences(context);
+        synchronized (PreferenceManager.class) {
+            pref.edit().putBoolean("statsNotification", enabled).apply();
+        }
+    }
+
+    public synchronized static int getLaudeValue(Context context){
+        setupSharedPreferences(context);
+        int laudeValue = Integer.parseInt(pref.getString(context.getResources().getString(R.string.key_default_laude), "30"));
+        if (laudeValue<30) laudeValue = 30;
+        return laudeValue;
+    }
+}
