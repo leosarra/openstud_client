f279f02750f56dc57d6fdfcc18aca91daf99b621
Leonardo Sarra <leonardosarra@outlook.com>
2019-02-24 12:59:05 +0100

    CustomCourse: Add model for custom course and lesson

---
+++ app/src/main/java/com/lithium/leona/openstud/data/CustomCourse.java
@@ -0,0 +1,100 @@
+package com.lithium.leona.openstud.data;
+
+import org.threeten.bp.DayOfWeek;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalTime;
+
+import java.util.List;
+
+public class CustomCourse {
+
+    public static class CustomLesson{
+        private DayOfWeek dayOfWeek;
+        private String where;
+        private LocalTime start;
+        private LocalTime end;
+
+
+        public CustomLesson() {
+
+        }
+
+        public DayOfWeek getDayOfWeek() {
+            return dayOfWeek;
+        }
+
+        public void setDayOfWeek(DayOfWeek dayOfWeek) {
+            this.dayOfWeek = dayOfWeek;
+        }
+
+        public String getWhere() {
+            return where;
+        }
+
+        public void setWhere(String where) {
+            this.where = where;
+        }
+
+        public LocalTime getStart() {
+            return start;
+        }
+
+        public void setStart(LocalTime start) {
+            this.start = start;
+        }
+
+        public LocalTime getEnd() {
+            return end;
+        }
+
+        public void setEnd(LocalTime end) {
+            this.end = end;
+        }
+    }
+
+    private String title;
+    private String teacher;
+    private LocalDate startCourse;
+    private LocalDate endCourse;
+    private List<CustomLesson> lessons;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<CustomLesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<CustomLesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    public LocalDate getStartCourse() {
+        return startCourse;
+    }
+
+    public void setStartCourse(LocalDate startCourse) {
+        this.startCourse = startCourse;
+    }
+
+    public LocalDate getEndCourse() {
+        return endCourse;
+    }
+
+    public void setEndCourse(LocalDate endCourse) {
+        this.endCourse = endCourse;
+    }
+}
