96c14547dfc61aa7e22e8d66b8271544cc8e75ad
Leonardo Sarra <leonardosarra@outlook.com>
2019-04-24 20:07:11 +0200

    ExamsWidget: Catch all exceptions when creating RemoteViews

--- app/src/main/java/com/lithium/leona/openstud/widgets/ExamsFactory.java
+++ app/src/main/java/com/lithium/leona/openstud/widgets/ExamsFactory.java
@@ -11,6 +11,7 @@ import android.widget.RemoteViewsService;
 
 import androidx.core.content.ContextCompat;
 
+import com.crashlytics.android.Crashlytics;
 import com.lithium.leona.openstud.R;
 import com.lithium.leona.openstud.data.InfoManager;
 import com.lithium.leona.openstud.helpers.WidgetHelper;
@@ -49,20 +50,25 @@ public class ExamsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     @Override
     public RemoteViews getViewAt(int position) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.item_exam_widget);
-        Event event = events.get(position);
-        remoteViews.setTextViewText(R.id.title, event.getTitle());
-        remoteViews.setTextViewText(R.id.countdown, "-" + WidgetHelper.getRemainingDays(event));
-        remoteViews.setTextViewText(R.id.teacherName, event.getTeacher());
-        String finalString = context.getResources().getString(R.string.cfu_date_widget, String.valueOf(event.getReservation().getCfu()), event.getEventDate().format(formatter));
-        SpannableString sb = new SpannableString(finalString);
-        sb.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.redSapienzaLight)), 0, finalString.indexOf("•"), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        sb.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.gray)), finalString.indexOf("•") + 1, finalString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        remoteViews.setTextViewText(R.id.cfu_date, sb);
-        if (!showCountdown) remoteViews.setViewVisibility(R.id.countdown, View.GONE);
-        else remoteViews.setViewVisibility(R.id.countdown, View.VISIBLE);
-        return remoteViews;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.item_exam_widget);
+            Event event = events.get(position);
+            remoteViews.setTextViewText(R.id.title, event.getTitle());
+            remoteViews.setTextViewText(R.id.countdown, "-" + WidgetHelper.getRemainingDays(event));
+            remoteViews.setTextViewText(R.id.teacherName, event.getTeacher());
+            String finalString = context.getResources().getString(R.string.cfu_date_widget, String.valueOf(event.getReservation().getCfu()), event.getEventDate().format(formatter));
+            SpannableString sb = new SpannableString(finalString);
+            sb.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.redSapienzaLight)), 0, finalString.indexOf("•"), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            sb.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.gray)), finalString.indexOf("•") + 1, finalString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            remoteViews.setTextViewText(R.id.cfu_date, sb);
+            if (!showCountdown) remoteViews.setViewVisibility(R.id.countdown, View.GONE);
+            else remoteViews.setViewVisibility(R.id.countdown, View.VISIBLE);
+            return remoteViews;
+        } catch (Exception e) {
+            Crashlytics.logException(e);
+        }
+        return null;
     }
 
     @Override
