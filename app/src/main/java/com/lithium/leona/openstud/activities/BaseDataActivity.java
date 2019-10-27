8d5e8408de669f17da282748ca2e24ff663f0224
Leonardo Sarra <leonardosarra@outlook.com>
2019-10-27 17:13:27 +0100

    BaseDataActivity: Recreate activity when theme changes

--- app/src/main/java/com/lithium/leona/openstud/activities/BaseDataActivity.java
+++ app/src/main/java/com/lithium/leona/openstud/activities/BaseDataActivity.java
@@ -3,7 +3,9 @@ package com.lithium.leona.openstud.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.lithium.leona.openstud.data.InfoManager;
+import com.lithium.leona.openstud.data.PreferenceManager;
 import com.lithium.leona.openstud.helpers.ClientHelper;
+import com.lithium.leona.openstud.helpers.ThemeEngine;
 
 import lithium.openstud.driver.core.Openstud;
 import lithium.openstud.driver.core.models.Student;
@@ -11,12 +13,13 @@ import lithium.openstud.driver.core.models.Student;
 abstract class BaseDataActivity extends AppCompatActivity {
     Student student;
     Openstud os;
-
+    ThemeEngine.Theme currentTheme;
     public BaseDataActivity() {
         super();
     }
 
     public boolean initData() {
+        currentTheme = ThemeEngine.resolveTheme(this, PreferenceManager.getTheme(this));
         os = InfoManager.getOpenStud(this);
         student = InfoManager.getInfoStudentCached(this, os);
         if (os == null || student == null) {
@@ -24,5 +27,12 @@ abstract class BaseDataActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        ThemeEngine.Theme newTheme = ThemeEngine.resolveTheme(this, PreferenceManager.getTheme(this));
+        if (newTheme != currentTheme) this.recreate();
     }
 }
